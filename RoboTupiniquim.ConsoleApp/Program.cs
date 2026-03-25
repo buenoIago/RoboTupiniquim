db063ae83c9084cb7fc933f3b7b9a7857e8634a6
buenoIago <iagocedup@gmail.com>
2026-03-25 15:17:33 -0300

    Refatorado

--- RoboTupiniquim.ConsoleApp/Program.cs
+++ RoboTupiniquim.ConsoleApp/Program.cs
@@ -1,5 +1,61 @@
 ﻿using System;
 
+static class Robo
+{
+    public static int roboX = 1;
+    public static int roboY = 2;
+    public static char direcao = 'N';
+
+    public static void Configurar(int rX, int rY, char dir)
+    {
+        rX = rX;
+        rY = rY;
+        dir = dir;
+    }
+
+    public static void ExecutarComando(string comando)
+    {
+        for (int i = 0; i < comando.Length; i++)
+        {
+            char comandoAtual = comando[i];
+
+            if (comandoAtual == 'E')
+            {
+                Robo.GirarParaEsquerda();
+            }
+            else if (comandoAtual == 'D')
+            {
+                Robo.GirarParaDireita();
+            }
+            else if (comandoAtual == 'M')
+            {
+                Robo.AvancarRobo();
+            }
+        }
+    }
+
+    public static void AvancarRobo()
+    {
+        if (direcao == 'N') roboY++;
+        else if (direcao == 'S') roboY--;
+        else if (direcao == 'L') roboY++;
+        else if (direcao == 'O') roboY--;
+    }
+    public static void GirarParaEsquerda()
+    {
+        if (direcao == 'N') direcao = 'O';
+        else if (direcao == 'O') direcao = 'S';
+        else if (direcao == 'S') direcao = 'L';
+        else if (direcao == 'L') direcao = 'N';
+    }
+    public static void GirarParaDireita()
+    {
+        if (direcao == 'N') direcao = 'L';
+        else if (direcao == 'O') direcao = 'N';
+        else if (direcao == 'S') direcao = 'L';
+        else if (direcao == 'L') direcao = 'S';
+    }
+}
 class Program
 {
     static void Main(string[] args)
@@ -17,47 +73,18 @@ class Program
         char direcao = Convert.ToChar(Console.ReadLine());
         */
 
-        int roboX = 1;
-        int roboY = 2;
-        char direcao = 'N';
-
-        string comando = "EMEMEMEMM";
+        Robo.Configurar(3, 3, 'L');
        
         Console.WriteLine("-------------------------------------"); 
         Console.WriteLine("Robô Tupiniquim");
         Console.WriteLine("-------------------------------------"); 
         Console.WriteLine("Coordenadas    X Y  Direção");
-        Console.WriteLine($"Posição inicial: {roboX} {roboY}   {direcao}");
+        Console.WriteLine($"Posição inicial: {Robo.roboX} {Robo.roboY}   {Robo.direcao}");
 
-        for (int i = 0; i < comando.Length; i++)
-        {
-            char comandoAtual = comando[i];
-
-            if (comandoAtual == 'E')
-            {
-                if (direcao == 'N') direcao = 'O';
-                else if (direcao == 'O') direcao = 'S';
-                else if (direcao == 'S') direcao = 'L';
-                else if (direcao == 'L') direcao = 'N';
-            }
-            else if (comandoAtual == 'D')
-            {
-                if (direcao == 'N') direcao = 'L';
-                else if (direcao == 'O') direcao = 'N';
-                else if (direcao == 'S') direcao = 'L';
-                else if (direcao == 'L') direcao = 'S';
-            }
-            else if (comandoAtual == 'M')
-            {
-                if (direcao == 'N') roboY++;
-                else if (direcao == 'S') roboY--;
-                else if (direcao == 'L') roboY++;
-                else if (direcao == 'O') roboY--;
-            }
-        }
+        Robo.ExecutarComando("EMEMEMEMM");
 
         Console.WriteLine("\nCoordenadas       X Y Direção");
-        Console.WriteLine($"Posição alcançada: {roboX} {roboY}   {direcao}");
+        Console.WriteLine($"Posição alcançada: {Robo.roboX} {Robo.roboY}   {Robo.direcao}");
         Console.ReadLine();
     }
 }
--- RoboTupiniquim.ConsoleApp/obj/Debug/net10.0/RoboTupiniquim.ConsoleApp.AssemblyInfo.cs
+++ RoboTupiniquim.ConsoleApp/obj/Debug/net10.0/RoboTupiniquim.ConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RoboTupiniquim.ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+415ee4d7dd33c86aea8700ba8b99dec9958c8767")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1bcfdab2cab0e1d24a0d838fcd44b346310bb1aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("RoboTupiniquim.ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RoboTupiniquim.ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
